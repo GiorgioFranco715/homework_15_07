c0f2e5b5506fe0f2e77898f97f578211a18e85cc
GiorgioFranco715 <138186314+GiorgioFranco715@users.noreply.github.com>
2023-07-17 17:05:17 +0300

    homework_15_07

---
+++ homework_15_07.py
@@ -0,0 +1,73 @@
+#if __name__ == "__main__":
+    #print("zdorov")
+
+    #Завдання 1
+    #Користувач вводить із клавіатури три числа.
+    # Залежно від вибору користувача програма виводить на екран суму трьох чисел або добуток трьох чисел.
+
+    #Ввідні дані від користувача
+
+    # number_one = int(input("Введіть перше число: "))
+    # number_two = int(input("Введіть друге число: "))
+    # number_three = int(input("Введіть третє число: "))
+    # choose_a_math_activity = input("Введіть слово Сума або Добуток:")
+
+
+    #Розв'язання задачі:
+
+    # if choose_a_math_activity.lower() =="сума":
+    #     print(number_one + number_two + number_three)
+    # elif choose_a_math_activity.lower() =="добуток":
+    #     print(number_one * number_two * number_three)
+    # else:
+    #     print("Ви ввели некоректні дані")
+
+    #Завдання 2
+    # Користувач вводить із клавіатури три числа.
+    # Залежно від вибору користувача програма виводить на екран максимум із трьох, мінімум із трьох або середньоарифметичне трьох чисел.
+
+    # Ввідні дані від користувача
+
+    # number_one = int(input("Введіть перше число: "))
+    # number_two = int(input("Введіть друге число: "))
+    # number_three = int(input("Введіть третє число: "))
+    # choose_a_math_activity = input("Введіть слово Максимум,Мінімум або Середньоарифметичне:")
+
+    # Розв'язання задачі:
+    # list_of_numbers = (number_one,number_two,number_three)
+    # if choose_a_math_activity.lower() =="максимум":
+    #     max_number = max(list_of_numbers)
+    #     print(max_number)
+    # elif choose_a_math_activity.lower() =="мінімум":
+    #     min_number = min(list_of_numbers)
+    #     print(min_number)
+    # elif choose_a_math_activity.lower() =="середньоарифметичне":
+    #     print((number_one+number_two+number_three)/3)
+    # else:
+    #     print("Ви ввели некоректні дані")
+
+    #Завдання 3
+
+    # Користувач вводить із клавіатури кількість метрів.
+    # Залежно від вибору користувача програма конвертує метри в милі, дюйми або ярди.
+
+    # Ввідні дані від користувача
+
+    # distanse_in_meters = int(input("Введіть дистанцію в метрах: "))
+    # units_of_measurement = input("Введіть в які одиниці виміру слід перевести дистанцію(В милі,В дюйми,В ярди): ")
+
+    # Розв'язання задачі:
+
+    # if units_of_measurement.lower() =="в милі":
+    #     print(distanse_in_meters * 0.00062137)
+    # elif units_of_measurement.lower() =="в дюйми":
+    #     print(distanse_in_meters * 39.37)
+    # elif units_of_measurement.lower() =="в ярди":
+    #     print(distanse_in_meters * 1.093613)
+    # else:
+    #     print("Ви ввели некоректні дані")
+
+
+
+
+
